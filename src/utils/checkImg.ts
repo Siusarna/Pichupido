9cba8bb75f2e21e15c0b957aa6b490aa874519a4
Vlad Hmara <hmara.vladislav2@gmail.com>
2020-11-06 10:56:56 +0200

    add additional checks

--- src/utils/checkImg.ts
+++ src/utils/checkImg.ts
@@ -1,15 +1,19 @@
 import Jimp from 'jimp';
 
 const checkHeightWidthSizeAndExtension = (img: Jimp, byteLength: number) => (
-  img.bitmap.height >= 512
+  img.bitmap.height >= 128
   && img.bitmap.height <= 1024
-  && img.bitmap.width >= 515
+  && img.bitmap.width >= 128
   && img.bitmap.height <= 1024
   && (byteLength / 1e6) <= 1
   && ['png', 'jpeg', 'jpg'].includes(img.getExtension()));
 
 export const checkImage = async (imgBuffer: string): Promise<boolean> => {
   const base64Data = Buffer.from(imgBuffer.replace(/^ ?data:image\/\w+;base64,/, ''), 'base64');
-  const img = await Jimp.read(base64Data);
-  return checkHeightWidthSizeAndExtension(img, base64Data.length);
+  try {
+    const img = await Jimp.read(base64Data);
+    return checkHeightWidthSizeAndExtension(img, base64Data.length); 
+  } catch (error) {
+    return false
+  }
 };
